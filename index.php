0c1ad37d896cac64f5843a77ac8d7621fd9d5cca
nozagiz4 <71056779+nozagiz4@users.noreply.github.com>
2021-01-11 15:38:43 +0700

    Update index.php

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <script src="js/bootstrap.min.js"></script>
     <script src="js/jquery-3.5.1.min.js"></script>
+    
     <link rel="stylesheet" href="/css/mystyle.css">
     <script src="js/default.js"></script>
 </head>
@@ -14,13 +15,12 @@
     <div class="container">
         Hello word <br>
     <img src="img/NOZAGI22.png" alt="" width="600px"><br><br>
-    <button class="btn btn-primary">
-            wawwwwww
-    </button>
+    <button class="btn btn-primary">wawwwwww</button>
+    <button class="btn btn-primary">wawwwwww</button>
     </div>
     <script>
         $(()=>{
-            alert("Hello");
+            //alert("Hello");
         });
 
     </script>
